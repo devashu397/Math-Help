f09289906cf6200581dd598815c8a1073feb6de6
Ashutosh Swamy <76511506+devashu397@users.noreply.github.com>
2022-12-03 18:57:19 +0530

    Package Version v1.2.0

---
+++ index.ts
@@ -0,0 +1,56 @@
+import {
+  add,
+  subtract,
+  multiply,
+  divide,
+  remainder,
+  exponent,
+} from "./functions/arithmetic";
+import {
+  rectangleArea,
+  squareArea,
+  triangleArea,
+  trapeziumArea,
+  parallelogramArea,
+  rhombusArea,
+  circleArea,
+} from "./functions/2dShapesArea";
+import {
+  cuboidSurfaceArea,
+  cubeSurfaceArea,
+  coneSurfaceArea,
+  cylinderSurfaceArea,
+  sphereSurfaceArea,
+} from "./functions/3dShapesSurfaceArea";
+import {
+  sectionPoint,
+  midPoint,
+  reflectInXAxis,
+  reflectInYAxis,
+  reflectInOrigin,
+} from "./functions/coordinateGeometry";
+
+export { add, subtract, multiply, divide, remainder, exponent };
+export {
+  rectangleArea,
+  squareArea,
+  triangleArea,
+  trapeziumArea,
+  parallelogramArea,
+  rhombusArea,
+  circleArea,
+};
+export {
+  cuboidSurfaceArea,
+  cubeSurfaceArea,
+  coneSurfaceArea,
+  cylinderSurfaceArea,
+  sphereSurfaceArea,
+};
+export {
+  sectionPoint,
+  midPoint,
+  reflectInXAxis,
+  reflectInYAxis,
+  reflectInOrigin,
+};
